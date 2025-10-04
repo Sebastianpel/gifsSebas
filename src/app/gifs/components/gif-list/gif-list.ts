5646b8daa00e2cdfebae9b087eeebe072ed34a05
Sebastianpel <sebastianparrapellaton@gmail.com>
2025-10-04 14:48:19 -0500

    sebas

--- src/app/gifs/components/gif-list/gif-list.ts
+++ src/app/gifs/components/gif-list/gif-list.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {GifListItem} from './gif-list-item/gif-list-item';
 
 @Component({
   selector: 'gif-list',
-  imports: [],
+  imports: [
+    GifListItem
+  ],
   templateUrl: './gif-list.html',
   styles: ``
 })
 export class GifList {
-
+  gifs = input.required<string[]>();
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,7 +5,8 @@ export const environment = {
   companySlogan: 'Maneja tus gifs',
 
   // ApiKeys
+  apiKey: 'Q5bGIT7au3jZwQNKQTbn5h1vaW98zVfm',
 
   //URLS
-
+  urlBase: 'https://api.giphy.com/v1',
 };
