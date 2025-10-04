5451a002014ac2cc08112dbb26ce9a8fb16e747b
Sebastianpel <sebastianparrapellaton@gmail.com>
2025-10-04 09:59:59 -0500

    a ver

--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {SideMenuOptions} from '../../components/side-menu/side-menu-options/side-menu-options';
-import {SideMenuHeader} from '../../components/side-menu/side-menu-header/side-menu-header';
+import {SideMenu} from '../../components/side-menu/side-menu';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [
     RouterOutlet,
-    SideMenuOptions,
-    SideMenuHeader
+    SideMenu
   ],
   templateUrl: './dashboard-page.html',
   styles: ``
